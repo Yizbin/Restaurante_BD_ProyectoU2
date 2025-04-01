93bc19b2d9fd79b651b3a8fbdf057de45fac517c
SEHUZ <sonic15622@gmail.com>
2025-03-31 23:05:09 -0700

    DAOs e interfaces para la capa de persistencia implementados (vacios)
    Creacion de una clase de utilerias para presentacion, se creo una pantalla del modulo de productos en presentacion (vacio). Se creo una carpeta img donde se agrego el logo del sistema

---
+++ RestaurantePresentacion/src/main/java/utilerias/UtileriasPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilerias;
+
+/**
+ *
+ * @author sonic
+ */
+public class UtileriasPresentacion {
+    public UtileriasPresentacion() {
+
+    }
+    
+}
