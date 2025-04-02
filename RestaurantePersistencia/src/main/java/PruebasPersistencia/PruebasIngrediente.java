f40a8a92c7b5d7d67d7d8c951b84a9ff66113bb6
Yizbin <abrahamcbringas@gmail.com>
2025-04-02 13:56:52 -0600

    Creacion de PruebasIngrediente
    En este main se probara todo lo relacionado a ingrediente.

---
+++ RestaurantePersistencia/src/main/java/PruebasPersistencia/PruebasIngrediente.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package PruebasPersistencia;
+
+import Exception.PersistenciaException;
+import ModuloIngredientesEntidades.Ingrediente;
+import moduloIngredientesDAO.IingredienteDAO;
+import moduloIngredientesDAO.IngredienteDAO;
+
+/**
+ *
+ * @author Abraham Coronel Bringas
+ */
+public class PruebasIngrediente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws PersistenciaException {
+        IingredienteDAO ingredienteDAO = IngredienteDAO.getInstanceDAO();
+        
+        //INGREDIENTES PRUEBAS
+        Ingrediente ingrediente = new Ingrediente("tortillas harina", 10, "piezas");
+        Ingrediente ingrediente1 = new Ingrediente("carne de burro", 10, "gr");
+        Ingrediente ingrediente2 = new Ingrediente("agua", 1000, "ml");
+        Ingrediente ingrediente3 = new Ingrediente("jamaica", 100, "piezas");
+        
+        ingredienteDAO.registrarIngrediente(ingrediente);
+        ingredienteDAO.registrarIngrediente(ingrediente1);
+        ingredienteDAO.registrarIngrediente(ingrediente2);
+        ingredienteDAO.registrarIngrediente(ingrediente3);
+    }
+
+}
